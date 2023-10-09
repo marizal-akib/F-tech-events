d8b6a5386b64f07889fbd06df1c5a43dc42ae593
marizal-akib <marizalakib@gmail.com>
2023-10-09 16:51:05 +0100

    set up login form

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,63 +1,87 @@
 import { Link } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import {FcGoogle} from 'react-icons/fc'
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(email, password);
+    
+  };
   return (
-    <div
-      className="hero min-h-screen"
-      style={{
-        backgroundImage:
-          "url(https://i.ibb.co/b5FpqVz/social-media-day-media-content-online-chat-multimedia-internet-sharimg-technology-message-likes-subs.png)",
-      }}
-    >
-      <div className="hero-overlay bg-blue-950 bg-opacity-70 "></div>
-      <div className="card-body max-w-5xl mx-auto card card-compact hover:bg-red-800 text-yellow-50
-          bg-red-900 bg-opacity-95 border border-fuchsia-950 shadow-xl ">
-        <h2 className=" text-2xl my-10 text-center">Login</h2>
+    <div>
+      <NavBar></NavBar>
 
-        <form className=" md:w-3 lg:w-4/5 mx-auto">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              name="email"
-              className="input input-bordered"
-              required
-            />
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/b5FpqVz/social-media-day-media-content-online-chat-multimedia-internet-sharimg-technology-message-likes-subs.png)",
+        }}
+      >
+        <div className="hero-overlay bg-blue-950 bg-opacity-70 "></div>
+        <div
+          className="card-body max-w-5xl mx-auto card card-compact hover:bg-red-800 text-black
+          bg-red-900 hover:bg-opacity-80 bg-opacity-80 border border-fuchsia-950 shadow-xl mt-20 mb-6 "
+        >
+          <h2 className=" text-4xl my-10 text-center text-amber-200">Login</h2>
+
+          <div className="flex flex-row items-center ">
+            <p className="font-semibold text-amber-100">Sign-In with :-</p>
+            <button className="btn  hover:bg-blue-500 bg-blue-700 bg-opacity-60 text-white border-fuchsia-950 "><FcGoogle className="text-xl"></FcGoogle> Google</button>
           </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              name="password"
-              className="input input-bordered"
-              required
-            />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
-          </div>
-        </form>
-        <p className="text-center mt-4">
-          Do not have an account? Please
-          <Link
-            className="text-blue-500 hover:text-blue-700 hover:underline"
-            to="/register"
-          >
-            Register
-          </Link>
-          .
-        </p>
+
+          <form onSubmit={handleLogin} className=" w-4/5 mx-auto">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn  hover:bg-blue-500 bg-blue-700 bg-opacity-60 text-white border-fuchsia-950">
+                Login
+              </button>
+            </div>
+          </form>
+          <p className="text-center mt-4">
+            Do not have an account? Please{" "}
+            <Link
+              className="text-blue-500 hover:text-blue-700 hover:underline"
+              to="/registration"
+            >
+              Register
+            </Link>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -1,13 +1,46 @@
 import NavBar from "../../NavBar/NavBar";
 
-
 const About = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <h2>About us</h2>
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/ngVS0JN/event-banner.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-blue-950 bg-opacity-70"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div>
+            <img
+              className="mt-5 md:max-w-sm w-2xl  "
+              src="https://i.ibb.co/fNHHwTH/Banner-Logo-removebg-preview-1-transformed.pnghttps://i.ibb.co/VVSL8yT/Banner-Logo-removebg-preview.png"
+              alt=""
+            />
+          </div>
         </div>
-    );
+      </div>
+      <div className=" bg-fuchsia-900 bg-opacity-20">
+        <div
+          className=" relative bottom-40 max-w-5xl mx-auto card card-compact hover:bg-red-800 text-yellow-50
+          bg-red-900 bg-opacity-95 border border-fuchsia-950 shadow-xl p-8"
+        >
+          <div className="card-body ">
+            <h2 className="text-5xl font-semibold text-center mx-auto card-title text-amber-200">About Us</h2>
+            <p className="text-center font-light text-xl mt-5 mx-auto w-4/5">We are passionate about crafting unforgettable experiences that leave a lasting impression. Our journey in the world of event management began with a vision to redefine the way events are conceived and executed. Today, we stand as a trusted partner for clients seeking innovation, expertise, and excellence in event planning.</p>
+
+            <h2 className="text-3xl font-semibold text-center mx-auto card-title mt-5 text-amber-200">Our Mission</h2>
+            <p className="text-center font-light text-xl mt-5 mx-auto w-4/5">Our mission is simple yet powerful: to transform ordinary gatherings into extraordinary moments. We believe that every event, whether it's a tech conference, gaming convention, or hackathon, has the potential to inspire, educate, and connect people. We're dedicated to making this potential a reality by delivering events that exceed expectations..</p>
+
+            <h2 className="text-3xl font-semibold text-center mx-auto card-title mt-5 text-amber-200">Our Team</h2>
+            <p className="text-center font-light text-xl mt-5 mx-auto w-4/5">Behind every successful event is a dedicated and passionate team. Our team is composed of event visionaries, logistics experts, tech enthusiasts, and creative minds who come together to bring your vision to life. We share a common goal: to create events that inspire, engage, and captivate.</p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
